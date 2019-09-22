b66c60aef356292adb6b5387fd9c65a2a76641b8
KevinTellier2 <54898019+KevinTellier2@users.noreply.github.com>
2019-09-22 10:30:36 +0200

    Update Projet R.R

--- Projet R.R
+++ Projet R.R
@@ -18,7 +18,8 @@ attach(dat)
 # On change le type de la variable de reponse "Class" (integer -> factor)
 dat$Class=as.factor(dat$Class)
 
-# Process de selection de variables les plus significatives
+
+#### Process de selection de variables les plus significatives ####
 set.seed(12345)
 
 # On change le type de la variable de reponse "Class" (integer -> factor)
@@ -30,7 +31,7 @@ summary(regs)
 # V17, V12 et V14 sont les variables les plus significatives
 dat.signif=dat[,c(18,15,13,31)]
 
-# On change le type de la variable de reponse "Class" (integer -> factor)
+
 # On change le type de la variable de reponse "Class" (integer -> factor)
 dat.signif$Class=as.factor(dat.signif$Class)
 
@@ -57,7 +58,7 @@ table(newdat$data$class) # 284315 Class=0
 
 
 
-###########################DEBUT#####################
+                ########################### DEBUT ###########################
 
 # Chargement de la table reechantillonnee
 
@@ -67,16 +68,17 @@ data$class=as.factor(data$class)
 set.seed(12345)
 data=data[,-1]
 
-## Debut de partitionnage Apprentissage / Test
+
+#### Debut de partitionnage Apprentissage / Test ####
 
 
 #Creation d'un echantillon d'apprentissage (70%) et test (30%) :
 ################NE PAS EXECUTER CE QUI SUIT  ############
 
 #index=1:nrow(data)
-#testindex=sample(index,397740)
-#train=data[testindex,]
-#test=data[-testindex,]
+#trainindex=sample(index,397740)
+#train=data[trainindex,]
+#test=data[-trainindex,]
 #attach(train)
 
 
@@ -95,41 +97,40 @@ attach(train)
 
 
 
-###########################################################
+######################################################################################
+######################################################################################
 
 
 
-
-###############  SVM kernel lineaire  ###############
+                    ###############  SVM kernel lineaire  ###############
 
 
 model=svm(class~.,data=train,kernel="linear",scale=F,cost=105)
-w <- t(model$coefs) %*% model$SV
+w = t(model$coefs) %*% model$SV
 
-#Taux de bonnes/mauvaises classifications sur echantillon test:
-
+# Taux de bonnes/mauvaises classifications sur echantillon TEST :
 Y=predict(model,newdata = test)
 
+table(test$class,Y)
+mean(test$class==Y) #91,4% de bonne classification
+mean(test$class!=Y) #8,6% de mauvaise classification
+
 #list(cbind(test, Y), model$index)
 
-table(test$class,Y)
-mean(test$class==Y)
-mean(test$class!=Y)
 
-
-
-#Taux de bonnes/mauvaises classifications sur echantillon complet:
-dataa=data[1:60000,]
+# Taux de bonnes/mauvaises classifications sur echantillon COMPLET :
+dataa=data[,] # Ici, on choisit le nombre de lignes qui définiera l'échantillon complet
 Y=predict(model,newdata = dataa)
 
+table(dataa$class,Y)
+mean(dataa$class==Y) # 92,3% de bonne classification
+mean(dataa$class!=Y) # 7,7% de mauvaise classification
+
 #list(cbind(test, Y), model$index)
 
-table(dataa$class,Y)
-mean(dataa$class==Y)
-mean(dataa$class!=Y)
 
 
-#Plot echantillon APPRENTISSAGE
+# Plot echantillon d'APPRENTISSAGE
 
 colors =c("blue","red")
 p3d<- plot3d(train$V12, train$V14, train$V17, xlab="V12", ylab="V14",
@@ -137,16 +138,17 @@ p3d<- plot3d(train$V12, train$V14, train$V17, xlab="V12", ylab="V14",
              col=as.integer(train$class) ,
              box=FALSE, size=5)
 
-text3d(train$V12, train$V14, train$V17, cex=0.5, adj = 1)
+#text3d(train$V12, train$V14, train$V17, cex=0.5, adj = 1)
 
-#Plot echantillon de TEST
+# Plot echantillon de TEST
 colors =c("blue","red")
 p3d<- plot3d(test$V12, test$V14, test$V17, xlab="V12", ylab="V14",
              zlab="V17",type="s",radius =0.3,
              col=as.integer(train$class) ,
              box=FALSE, size=5)
 
-text3d(test$V12, test$V14, test$V17, cex=0.5, adj = 1)
+# text3d(test$V12, test$V14, test$V17, cex=0.5, adj = 1)
+
 
 length = 100                                                                                                                                                                 
 grid = expand.grid(seq(from=min(train$V17),to=max(train$V17),length.out=length),                                                                                                         
@@ -154,68 +156,74 @@ grid = expand.grid(seq(from=min(train$V17),to=max(train$V17),length.out=length),
 z = (model$rho- w[1,1]*grid[,1] - w[1,2]*grid[,2]) / w[1,3]
 #z=(w[1,1]*grid[,1] + w[1,2]*grid[,2])
 
-plot3d(grid[,1],grid[,2],z)  # this will draw the plane
-# adding of points to the graphics.
+
+# Drawing the plane &nd adding points to the graphic
+plot3d(grid[,1],grid[,2],z)
+
 points3d(train$V17[which(train$class==0)], train$V14[which(train$class==0)], train$V12[which(train$class==0)], col='red')
 points3d(train$V17[which(train$class==1)], train$V14[which(train$class==1)], train$V12[which(train$class==1)], col='blue')
 
+prediction=predict(model, test[,-4])
+(tab=table(pred = prediction, true=test[,4]))
 
 
 
-###############  Optimal Gamma/Cost (Ne pas exÃ©cuter Ã  chaque session, on a dÃ©jÃ  les rÃ©sultats) ###############
+##########  Optimal Gamma/Cost (Ne pas executer a chaque session, on a deja les resultats) ##########
 
 
-#10 Fold cross validation to determine the optimal gamma and C (cost) in the SVM
-#Best parameters : Gamma : 0.01  / Cost (C) : 10
+# 10 Fold cross validation to determine the optimal gamma and C (cost) in the SVM
+
 attach(train)
 tuned = tune.svm(class~., data = train, gamma = 10^(-6:-1), cost = 10^(-1:1))
 summary(tuned)
 
+# Best parameters : Gamma : 0.01  / Cost (C) : 10
 gamma=0.01
 C=10
 
 
 
-###############  SVM kernel Radial Basis  ###############
+                    ###############  SVM kernel Radial Basis  ###############
 
 
 model=svm(class~., data=train, kernel="radial",gamma = 0.01, cost = 10) 
-w <- t(model$coefs) %*% model$SV
+w = t(model$coefs) %*% model$SV
 
 
-####Taux de bonne prediction sur echantillon test
+#### Taux de bonne prediction sur echantillon TEST
 Y=predict(model,newdata = test)
 table(test$class,Y)
-mean(test$class==Y)
-mean(test$class!=Y)
+mean(test$class==Y) # 91,4% de bonne classification
+mean(test$class!=Y) # 8,6% de mauvaise classification
 
-####Taux de bonnes/mauvaises classifications sur echantillon complet:
-dataa=data[1:60000,]
+
+#### Taux de bonnes/mauvaises classifications sur echantillon COMPLET :
+dataa=data[,] # Ici, on choisit le nombre de lignes qui définiera l'échantillon complet
 Y=predict(model,newdata = dataa)
 
 table(dataa$class,Y)
-mean(dataa$class==Y)
-mean(dataa$class!=Y)
+mean(dataa$class==Y) # 92,2% de bonne classification
+mean(dataa$class!=Y) # 7,8% de mauvaise classification
 
 
-#Plot echantillon APPRENTISSAGE
+# Plot echantillon d'APPRENTISSAGE
 
 colors =c("blue","red")
-p3d<- plot3d(train$V12, train$V14, train$V17, xlab="V12", ylab="V14",
+p3d = plot3d(train$V12, train$V14, train$V17, xlab="V12", ylab="V14",
              zlab="V17",type="s",radius =0.3,
              col=as.integer(train$class) ,
              box=FALSE, size=5)
 
-text3d(train$V12, train$V14, train$V17, cex=0.5, adj = 1)
+# text3d(train$V12, train$V14, train$V17, cex=0.5, adj = 1)
 
-#Plot echantillon de TEST
+# Plot echantillon de TEST
 colors =c("blue","red")
-p3d<- plot3d(test$V12, test$V14, test$V17, xlab="V12", ylab="V14",
+p3d = plot3d(test$V12, test$V14, test$V17, xlab="V12", ylab="V14",
              zlab="V17",type="s",radius =0.3,
              col=as.integer(train$class) ,
              box=FALSE, size=5)
 
-text3d(test$V12, test$V14, test$V17, cex=0.5, adj = 1)
+# text3d(test$V12, test$V14, test$V17, cex=0.5, adj = 1)
 
 
 
@@ -225,54 +233,58 @@ grid = expand.grid(seq(from=min(train$V17),to=max(train$V17),length.out=length),
                    seq(from=min(train$V14),to=max(train$V14),length.out=length))                                                                                                         
 z= exp((-gamma)*(w[1,1]*grid[,1] - w[1,2]*grid[,2])^2) ##(model$rho-*w[1,1]*grid[,1] - w[1,2]*grid[,2])^2)
 
+# Drawing the plane &nd adding points to the graphic
 plot3d(grid[,1],grid[,2],z,aspect = F)
 
 points3d(train$V17[which(train$class==0)], train$V14[which(train$class==0)], train$V12[which(train$class==0)], col='red')
 points3d(train$V17[which(train$class==1)], train$V14[which(train$class==1)], train$V12[which(train$class==1)], col='blue')
 
+prediction=predict(model, test[,-4])
+(tab=table(pred = prediction, true=test[,4]))
 
 
 
-###############  SVM kernel Polynomial  ###############
+
+                    ###############  SVM kernel Polynomial  ###############
 
 
 model=svm(class~., data=train, kernel="polynomial",gamma = 0.01, cost = 10) 
-w <- t(model$coefs) %*% model$SV
+w = t(model$coefs) %*% model$SV
 
 
-####Taux de bonne prediction sur echantillon test
+#### Taux de bonne prediction sur echantillon TEST
 Y=predict(model,newdata = test)
 table(test$class,Y)
-mean(test$class==Y)
-mean(test$class!=Y)
+mean(test$class==Y) # 64,6% de bonne classification
+mean(test$class!=Y) # 35,4% de mauvaise classification
 
-####Taux de bonnes/mauvaises classifications sur echantillon complet:
-dataa=data[1:60000,]
+#### Taux de bonnes/mauvaises classifications sur echantillon COMPLET :
+dataa=data[,]  # Ici, on choisit le nombre de lignes qui définiera l'échantillon complet
 Y=predict(model,newdata = dataa)
 
 table(dataa$class,Y)
-mean(dataa$class==Y)
-mean(dataa$class!=Y)
+mean(dataa$class==Y) # 63,4% de bonne classification
+mean(dataa$class!=Y) # 36,6% de mauvaise classification
 
 
-#Plot echantillon APPRENTISSAGE
+# Plot echantillon d'APPRENTISSAGE
 
 colors =c("blue","red")
-p3d<- plot3d(train$V12, train$V14, train$V17, xlab="V12", ylab="V14",
+p3d = plot3d(train$V12, train$V14, train$V17, xlab="V12", ylab="V14",
              zlab="V17",type="s",radius =0.3,
              col=as.integer(train$class) ,
              box=FALSE, size=5)
 
-text3d(train$V12, train$V14, train$V17, cex=0.5, adj = 1)
+# text3d(train$V12, train$V14, train$V17, cex=0.5, adj = 1)
 
-#Plot echantillon de TEST
+# Plot echantillon de TEST
 colors =c("blue","red")
-p3d<- plot3d(test$V12, test$V14, test$V17, xlab="V12", ylab="V14",
+p3d = plot3d(test$V12, test$V14, test$V17, xlab="V12", ylab="V14",
              zlab="V17",type="s",radius =0.3,
              col=as.integer(train$class) ,
              box=FALSE, size=5)
 
-text3d(test$V12, test$V14, test$V17, cex=0.5, adj = 1)
+# text3d(test$V12, test$V14, test$V17, cex=0.5, adj = 1)
 
 
 
@@ -282,8 +294,9 @@ grid = expand.grid(seq(from=min(train$V17),to=max(train$V17),length.out=length),
                    seq(from=min(train$V14),to=max(train$V14),length.out=length))                                                                                                         
 z=((gamma*(w[1,1]*grid[,1] + w[1,2]*grid[,2])+model$coef0))^2
 
-plot3d(grid[,1],grid[,2],z)  # this will draw plane.
-# adding of points to the graphics.
+# Drawing the plane &nd adding points to the graphic
+plot3d(grid[,1],grid[,2],z)  
+
 points3d(train$V17[which(train$class==0)], train$V14[which(train$class==0)], train$V12[which(train$class==0)], col='red')
 points3d(train$V17[which(train$class==1)], train$V14[which(train$class==1)], train$V12[which(train$class==1)], col='blue')
 
@@ -294,45 +307,45 @@ prediction=predict(model, test[,-4])
 
 
 
-###############  SVM kernel sigmoid  ###############
+                    ###############  SVM kernel sigmoid  ###############
 
 model=svm(class~., data=train, kernel="sigmoid",gamma = 0.01, cost = 10) 
-w <- t(model$coefs) %*% model$SV
+w = t(model$coefs) %*% model$SV
 
 
-####Taux de bonne prediction sur echantillon test
+#### Taux de bonne prediction sur echantillon TEST
 Y=predict(model,newdata = test)
 table(test$class,Y)
-mean(test$class==Y)
-mean(test$class!=Y)
+mean(test$class==Y) # 91,5% de bonne classification
+mean(test$class!=Y) # 8,5% de mauvaise classification
 
-####Taux de bonnes/mauvaises classifications sur echantillon complet:
-dataa=data[1:60000,]
+#### Taux de bonnes/mauvaises classifications sur echantillon COMPLET :
+dataa=data[,] # Ici, on choisit le nombre de lignes qui définiera l'échantillon complet
 Y=predict(model,newdata = dataa)
 
 table(dataa$class,Y)
-mean(dataa$class==Y)
-mean(dataa$class!=Y)
+mean(dataa$class==Y) # 92,2% de bonne classification
+mean(dataa$class!=Y) # 7,8% de mauvaise classification
 
 
-#Plot echantillon APPRENTISSAGE
+# Plot echantillon d'APPRENTISSAGE
 
-colors =c("blue","red")
-p3d<- plot3d(train$V12, train$V14, train$V17, xlab="V12", ylab="V14",
+colors=c("blue","red")
+p3d = plot3d(train$V12, train$V14, train$V17, xlab="V12", ylab="V14",
              zlab="V17",type="s",radius =0.3,
              col=as.integer(train$class) ,
              box=FALSE, size=5)
 
-text3d(train$V12, train$V14, train$V17, cex=0.5, adj = 1)
+# text3d(train$V12, train$V14, train$V17, cex=0.5, adj = 1)
 
-#Plot echantillon de TEST
-colors =c("blue","red")
-p3d<- plot3d(test$V12, test$V14, test$V17, xlab="V12", ylab="V14",
+# Plot echantillon de TEST
+colors=c("blue","red")
+p3d = plot3d(test$V12, test$V14, test$V17, xlab="V12", ylab="V14",
              zlab="V17",type="s",radius =0.3,
              col=as.integer(train$class) ,
              box=FALSE, size=5)
 
-text3d(test$V12, test$V14, test$V17, cex=0.5, adj = 1)
+# text3d(test$V12, test$V14, test$V17, cex=0.5, adj = 1)
 
 
 
@@ -342,6 +355,8 @@ grid = expand.grid(seq(from=min(train$V17),to=max(train$V17)),
                    seq(from=min(train$V14),to=max(train$V14)))                                                                                                         
 
 z=tanh((gamma*(w[1,1]*grid[,1] + w[1,2]*grid[,2]))+model$coef0)
+
+# Drawing the plane &nd adding points to the graphic
 plot3d(grid[,1],grid[,2],z,aspect = F)
 
 points3d(train$V17[which(train$class==0)], train$V14[which(train$class==0)], train$V12[which(train$class==0)], col='red')
@@ -357,3 +372,4 @@ prediction=predict(model, test[,-4])
 #Sigmoid ?
 
 #formule ligne 226 ?
+#ligne 345, col=colors non ?
